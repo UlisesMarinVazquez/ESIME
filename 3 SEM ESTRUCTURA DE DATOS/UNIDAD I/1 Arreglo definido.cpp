2cb338a1c421f0cd422077490a3d8d5a72f191d5
UlisesMarinVazquez <ulisesmavz@gmail.com>
2023-09-24 12:33:21 -0600

    redit

---
+++ 3 SEM ESTRUCTURA DE DATOS/UNIDAD I/1 Arreglo definido.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+void arrayDefinido(int[]);
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5};
+    arrayDefinido(array);
+
+    return 0;
+};
+
+void arrayDefinido(int arr[])
+{
+    cout << "Mi arreglo es: \n";
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
